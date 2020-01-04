350a99988312fd8a094eb5b7011d0bc7a6ea2825
rnavarro <rod.nvrr@gmail.com>
2020-01-04 16:29:09 -0300

    main.go modified - http server added

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	// creates a new Multiplexor server
+	// a request multiplexer matches the URL of incoming requests against
+	// a list of registered paths and calls the associated handler for the path whenever a match is found.
+	mux := http.NewServeMux()
+
+	// Handle function for the root path "/"
+	mux.HandleFunc("/", indexHandler)
+
+	// starts the server using "port" var parameters
+	http.ListenAndServe(":"+port, mux)
+}
