043b4f22beaf68a365e5e0de22a0bd2a4cd23069
rnavarro <rod.nvrr@gmail.com>
2020-01-05 18:13:36 -0300

    json encode added, search logic added

--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
+	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+
+var apiKey *string
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+	SearchKey  string
+	Nextpage   int
+	TotalPages int
+	Results    Results
+}
+
+func (a *Article) FormatPublishedDate() string {
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v %d, %d", month, day, year)
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("<h1>Hello World! - 1st webserver with Go</h1>"))
@@ -27,7 +69,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	params := u.Query()
-	searchKey := params.Get("query")
+	searchKey := params.Get("q")
 	page := params.Get("page")
 	if page == "" {
 		page = "1"
@@ -35,9 +77,60 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Search Query is: ", searchKey)
 	fmt.Println("Results page is: ", page)
+
+	// new inst of "Search" struct
+	search := &Search{}
+	search.SearchKey = searchKey
+
+	// convert "page" into an integer "next"
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		return
+	}
+
+	// "next" being assigned to "Nextpage"
+	search.Nextpage = next
+	pageSize := 20 // can be 0 to 100
+
+	endpoint := fmt.Sprintf(
+		"https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.Nextpage, *apiKey)
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / (pageSize))))
+
+	err = tpl.Execute(w, search)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func main() {
+	apiKey = flag.String("apiKey", "", "Newsapi.org access key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("apiKey must be set")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
